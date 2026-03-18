ad4bd6ab5286426966ef2c7ebb697111c609dca4
adrianaarang <129066063+adrianaarang@users.noreply.github.com>
2026-03-18 11:30:42 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,19 @@
+import pandas as pd
 import streamlit as st
+import requests
 
-st.title("FUNCIONA")
+st.title("📊 Job Trends Analyzer")
+
+url = "https://remotive.com/api/remote-jobs"
+
+try:
+    response = requests.get(url)
+    response.raise_for_status()
+    data = response.json()
+    df = pd.DataFrame(data["jobs"])
+
+    st.write("Ofertas encontradas:", len(df))
+    st.dataframe(df[["title", "company_name"]].head(20))
+
+except Exception as e:
+    st.error(f"Error cargando datos: {e}")
